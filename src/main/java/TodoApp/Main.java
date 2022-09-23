69a0d8290e37a44a367dd74ada95afd9865d1fcf
GL-19 <loiolagl19@gmail.com>
2022-09-23 16:14:13 -0300

    feat: created ConnectionFactory Class

---
+++ src/main/java/TodoApp/Main.java
@@ -0,0 +1,11 @@
+package TodoApp;
+
+import java.sql.Connection;
+import util.ConnectionFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection c = ConnectionFactory.getConnection();
+        
+    }
+}
